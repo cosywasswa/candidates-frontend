e9232ddacfdbde9a52ce3e435367f932a77ff32c
Cosmas Wasswa <cosywas@gmail.com>
2025-11-03 21:31:58 +0300

    updated with filter functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { useUser } from "./authentication/context";
 import { fetchCandidates } from "./redux/candidateSlice";
 
 const Home = () => {
-  const [tierDescription, setTierDescription] = useState("")
+  const [searchTier, setSearchTier] = useState("")
   const {candidates} = useSelector((store) => store.candidatesData);
   const { user } = useUser();
   const dispatch = useDispatch();
@@ -14,37 +14,45 @@ const Home = () => {
   }, []);
 
   console.log('candidates', candidates)
-  const candidatesArr = candidates?.data
+  const candidatesArr = candidates?.data ||[]
+
+  const filteredCandidates = candidatesArr.filter((candidate)=>candidate.tier.toString().includes(searchTier))
 
   return (
    <main className="bg-slate-200 w-screen min-h-screen flex flex-col">
   <h1 className="text-lg md:text-4xl text-center pt-10">
     Candidate Categorization Platform
   </h1>
-
-  <section className="flex flex-col items-center gap-6 pt-20">
-    {candidates?.data &&
-      candidates?.data.map((candidate) => (
-        
-        <Link
-  key={candidate._id}
-  to={`/home/${candidate._id}`}
-  className="flex justify-between items-center w-3/4 bg-white p-5 rounded-lg shadow-md hover:shadow-lg transition"
->
-  <div className="flex gap-2 w-1/2">
-    <p className="font-bold">Name:</p>
-    <span>{candidate.name}</span>
+  <div className="pt-5">
+    <input type="text" placeholder="search by tier" className="outline-none bg-white rounded-2xl px-2 relative left-44 top-5 w-60"
+    value={searchTier}
+    onChange={(e)=>setSearchTier(e.target.value)}
+    />
   </div>
 
-  <div className="flex gap-2 w-1/2 justify-end">
-    <p className="font-bold">Tier:</p>
-    <span>{candidate.tier}</span>
-  </div>
-</Link>
+  <section className="flex flex-col items-center gap-6 pt-10">
+  {filteredCandidates.length > 0 ? (
+    filteredCandidates.map((candidate) => (
+      <Link
+        key={candidate._id}
+        to={`/home/${candidate._id}`}
+        className="flex justify-between items-center w-3/4 bg-white p-5 rounded-lg shadow-md hover:shadow-lg transition"
+      >
+        <div className="flex gap-2 w-1/2">
+          <p className="font-bold">Name:</p>
+          <span>{candidate.name}</span>
+        </div>
 
-       
-      ))}
-  </section>
+        <div className="flex gap-2 w-1/2 justify-end">
+          <p className="font-bold">Tier:</p>
+          <span>{candidate.tier}</span>
+        </div>
+      </Link>
+    ))
+  ) : (
+    <p className="text-gray-500 mt-4">No candidates found for this tier.</p>
+  )}
+</section>
 </main>
 
   );
